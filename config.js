5f420457915c4e1d6e7c57d73c219d6f74739179
debguin1-alt <debguin1-alt@users.noreply.github.com>
2026-03-23 11:52:34 +0000

    deploy: update site [skip ci] 7597ef139019d1b6bff23f485db6087e5faffeaf

---
+++ config.js
@@ -0,0 +1,7 @@
+window.__NX_CONFIG__ = {
+  apiKey:            "AIzaSyCoPJjVzwaaZBgvz-P4vGJ44muNS51qqeg",
+  authDomain:        "tuition-fee-management-4e15e.firebaseapp.com",
+  projectId:         "tuition-fee-management-4e15e",
+  messagingSenderId: "1093461316314",
+  appId:             "1:1093461316314:web:c06be1b4165f0cfadc0be3"
+};
